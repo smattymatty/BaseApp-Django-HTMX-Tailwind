52a0d4c20914055b1e693683091ebbde764c9fbc
smattymatty <109621208+smattymatty@users.noreply.github.com>
2024-05-31 01:21:53 -0400

    locust tests

--- locust_tests.py
+++ locust_tests.py
@@ -13,3 +13,29 @@ class WebsiteUser(HttpUser):
         self.client.get("/base/ui-elements/")  # Relative path
 
     # ... other tasks
+
+
+class BlogUser(HttpUser):
+    wait_time = between(1, 5)  # Simulate user think time
+
+    @task(1)
+    def basic_blog_post_list(self):
+        # Get the first page (default)
+        self.client.post("/blog/blog-post-list/", data={"page": 1})
+
+    @task(2)
+    def next_page_blog_post_list(self):
+        # Get a subsequent page
+        self.client.post("/blog/blog-post-list/", data={"page": 2})
+
+    @task(3)
+    def search_blog_posts(self):
+        search_term = "western"  # Or generate random search terms
+        self.client.post("/blog/blog-post-list/",
+                         data={"page": 1, "search_query": search_term})
+
+    @task(4)
+    def search_blog_posts_with_pagination(self):
+        search_term = "black"  # Or generate random search terms
+        self.client.post("/blog/blog-post-list/",
+                         data={"page": 2, "search_query": search_term})
