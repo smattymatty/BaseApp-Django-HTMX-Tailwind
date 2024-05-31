86007f7457d25ced560de44a6c2a88a901506cce
smattymatty <109621208+smattymatty@users.noreply.github.com>
2024-05-31 00:52:26 -0400

    remove rest framework

--- core/settings.py
+++ core/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     "django_htmx",
     'BaseApp',
     'UsersApp',
