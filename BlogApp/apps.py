389e5009cdbf14b7020cdcf025400a9232b9d4ee
smattymatty <109621208+smattymatty@users.noreply.github.com>
2024-05-31 03:04:20 -0400

    signals?

--- BlogApp/apps.py
+++ BlogApp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BlogappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'BlogApp'
+
+    def ready(self):
+        import BlogApp.signals
