163fc778cd5b7a5b66ab9f978aad5b1f665d81e1
smattymatty <109621208+smattymatty@users.noreply.github.com>
2024-06-25 14:08:37 -0400

    save it all

--- FlashCardApp/urls.py
+++ FlashCardApp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.FlashCardAppView.as_view(), name="base"),
     path("decks/", views.FlashCardAppView.deck_list, name="deck_list"),
+    path("decks/<int:deck_id>/",
+         views.FlashCardAppView.deck_detail, name="deck_detail"),
 ]
--- FlashCardApp/views.py
+++ FlashCardApp/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from BaseApp.views import BasePage
 from BaseApp.utils import require_htmx
+
 from .models import Deck
 
 
 class FlashCardAppView(BasePage):
     """
     The main view for the FlashCardApp
-    This is a container for all of it's staticmethod views
-    Usage in urls.py:
-        path('', FlashCardAppView.as_view(), name="base"),
-        path('decks/', FlashCardAppView.deck_list, name="deck_list"),
     """
     template_name = "FlashCardApp/base.html"
     title = "Flash Card App"
@@ -20,7 +18,7 @@ class FlashCardAppView(BasePage):
     @require_htmx
     def deck_list(request):
         """
-        This view is used to display a list of all decks in the database
+        Display a list of all decks in the database
         """
         context = {
             'decks': Deck.objects.all()
@@ -28,5 +26,20 @@ class FlashCardAppView(BasePage):
         return render(
             request,
             'FlashCardApp/sections/deck_list.html',
+            context
+        )
+
+    @staticmethod
+    @require_htmx
+    def deck_detail(request, deck_id):
+        """
+        Display the details of a specific deck
+        """
+        context = {
+            'deck': get_object_or_404(Deck, pk=deck_id)
+        }
+        return render(
+            request,
+            'FlashCardApp/sections/deck_detail.html',
             context
         )
